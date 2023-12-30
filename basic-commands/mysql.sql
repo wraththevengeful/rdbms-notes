e08f167edbc861bcf3fad355874766c3d84fa213
Elanchezhiyan <elanchezhiyan2440@gmail.com>
2023-12-30 11:55:40 +0530

    updated gitignore to leave out my notes folder

--- basic-commands/mysql.sql
+++ basic-commands/mysql.sql
@@ -1,11 +1,12 @@
---Show the list of databases in your server:
-SHOW DATABASES;
+--MYSQL IS CASE SENSITIVE IN TERMS OF NAMING TABLES AND ATTRIBUTES
+--SO PLEASE ALWAYS USE CAPS LOCK ON AND TYPE EVERYTHING IN CAPS
+
+--******************************************************************************************************
+--USER COMMANDS:
 
 --Show the list of users in your server:
 SELECT user, host FROM mysql.user;
 
---******************************************************************************************************
---CREATING AN USER:
 -- Create a new user
 CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
 
@@ -14,7 +15,14 @@ GRANT ALL PRIVILEGES ON database_name.* TO 'newuser'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
+
+--See current user:
+SELECT USER();
+
 --*****************************************************************************************************
+--DATABASE COMMANDS:
+--Show the list of databases in your server:
+SHOW DATABASES;
 
 -- Create a new database
 CREATE DATABASE your_database_name;
@@ -25,5 +33,14 @@ USE your_database_name;
 -- Drop a database (Delete):
 DROP DATABASE your_database_name;
 
+--view all databases:
+SHOW DATABASES;
+
 -- Show your current database:
 SELECT DATABASE();
+
+--Show tables of your current database:
+SHOW TABLES;
+
+--View details of a specific table:
+DESCRIBE your_table_name;
---
+++ ques-no-7/data-ques-no-7.sql
@@ -0,0 +1,12 @@
+--CUSTOMER TABLE:
+
+INSERT INTO CUSTOMER (CNAME)VALUES ('Aarav'), ('Aisha'), ('Akshay'), ('Anaya'), ('Arjun'), ('Bhavya'), ('Chetan'), ('Deepika'), ('Dev'), ('Esha'),
+('Farhan'), ('Gayatri'), ('Hrithik'), ('Ishaan'), ('Jaya'), ('Kunal'), ('Lavanya'), ('Manish'), ('Nandini'), ('Omkar'),
+('Pooja'), ('Rahul'), ('Reeva'), ('Sahil'), ('Shruti'), ('Tanvi'), ('Uday'), ('Vaishnavi'), ('Yuvraj'), ('Zara'),
+('Advait'), ('Alisha'), ('Anirudh'), ('Bhumi'), ('Chirag'), ('Diya'), ('Eeshan'), ('Gargi'), ('Harsh'), ('Ira'),
+('Jatin'), ('Kashvi'), ('Laksh'), ('Meera'), ('Neha'), ('Omisha'), ('Pranav'), ('Radhika'), ('Sameer'), ('Tanisha'),
+('Vedant'), ('Aarushi'), ('Ankit'), ('Bhavana'), ('Chaitanya'), ('Divya'), ('Eknath'), ('Gautami'), ('Hema'), ('Ishan'),
+('Jhanvi'), ('Karan'), ('Mira'), ('Niharika'), ('Ojas'), ('Pradeep'), ('Rajni'), ('Sanjay'), ('Tanya'), ('Vikram'),
+('Advika'), ('Amol'), ('Anuja'), ('Bhuvan'), ('Charul'), ('Dinesh'), ('Ekta'), ('Gopal'), ('Harsha'), ('Isha'), ('Jyoti'),
+('Kavya'), ('Madhav'), ('Nidhi'), ('Omisha'), ('Prateek'), ('Ravi'), ('Shivangi'), ('Tia'), ('Vivan'), ('Advith'), ('Amrita'),
+('Arpita'), ('Biren'), ('Chhaya'), ('Darsh'), ('Eesha'), ('Gaurav'), ('Hetal'), ('Ira');
